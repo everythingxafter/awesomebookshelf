d512dc37be7eefdf68c4b231b21efc5b9f9f253f
unknown <shobrinaf@gmail.com>
2023-03-12 08:42:01 +0700

    return the dashboard to the preliminary view

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,52 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
+import React from "react";
 import DashboardNavbar from "./component/DashboardNavbar";
 import "./Dashboard.css";
-import axios from "axios";
 
 export default function Dashboard() {
-  const navigate = useNavigate();
-  const random = Math.floor(Math.random() * 11);
-  const [preliminaryData, setPreliminaryData] = useState({});
-
-  const onClick = () => {
-    navigate("/updatebook", {
-      state: {
-        id: random,
-        preliminaryData,
-      },
-    });
-  };
-
-  useEffect(() => {
-    const configGet = {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    };
-
-    const getData = async () => {
-      try {
-        const data = await axios.get(
-          `//localhost:8080/content/data/${random}`,
-          configGet
-        );
-        setPreliminaryData(data.data);
-        console.log(data.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getData();
-  }, [random]);
-
   return (
     <div>
       <DashboardNavbar />
       <p>ini Dashboard</p>
-      <button onClick={onClick}>goTO update</button>
     </div>
   );
 }
