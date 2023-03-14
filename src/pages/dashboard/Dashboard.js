cf9558a6164203fd4a8165abdc36099182b4902a
everythingxafter <112627979+everythingxafter@users.noreply.github.com>
2023-03-15 00:36:46 +0700

    Update Dashboard.js

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,12 +1,19 @@
 import React from "react";
-import DashboardNavbar from "./component/DashboardNavbar";
+import { Container } from "react-bootstrap";
+import NavbarSearch from "../../component/navbar/NavbarSearch";
+import NavbarSide from "../../component/navbar/NavbarSide";
 import './Dashboard.css'
 
 export default function Dashboard() {
     return (
-        <div>
-            <DashboardNavbar />
-            <p>ini Dashboard</p>
+        <div style={{ display: 'flex' }}>
+            <NavbarSide />
+            <div style={{ width: '100%' }}>
+                <NavbarSearch />
+                <Container style={{ marginTop: '3em', marginBottom: '2em', color: 'var(--fontColorPrimary)' }}>
+                    <h1>Your book</h1>
+                </Container>
+            </div>
         </div>
     )
 }
