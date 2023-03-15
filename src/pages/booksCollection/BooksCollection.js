cfe9c98706fee9584bb9cbfb536ae2f3d5990d6d
unknown <shobrinaf@gmail.com>
2023-03-15 22:50:49 +0700

    connect reading list to db but still error

--- src/pages/booksCollection/BooksCollection.js
+++ src/pages/booksCollection/BooksCollection.js
@@ -8,11 +8,14 @@ import axios from "axios";
 
 export default function BooksCollection() {
   const [data, setData] = useState([]);
+  const [dataAdd, setDataAdd] = useState({});
+//  const [add, setAdd] = useState(false)
+  const access_token = localStorage.getItem("access_token");
   const dataPopular = data?.sort((a, b) => {
     return b.PageViews - a.PageViews;
   });
 
-  const dataCarousel = dataPopular.slice(0, 3)
+  const dataCarousel = dataPopular.slice(0, 3);
 
   const getData = async () => {
     try {
@@ -24,8 +27,24 @@ export default function BooksCollection() {
   };
 
   useEffect(() => {
+    const config = {
+      headers: {
+        access_token,
+      },
+    };
+
+    const getAdd = async () => {
+      try {
+        const res = await axios.get("//localhost:5000/readinglist", config);
+        setDataAdd(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     getData();
-  }, []);
+    getAdd();
+  }, [access_token]);
 
   return (
     <div style={{ display: "flex" }}>
@@ -33,7 +52,7 @@ export default function BooksCollection() {
       <div style={{ width: "100%" }}>
         <NavbarSearch />
         <CarouselCollection data={dataCarousel} />
-        <PopularBook data={dataPopular} />
+        <PopularBook data={dataPopular} dataReadingList={dataAdd} />
       </div>
     </div>
   );
--- src/pages/booksCollection/component/PopularBook.js
+++ src/pages/booksCollection/component/PopularBook.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import PopularBookCard from "./PopularBookCard";
-import './PopularBook.css'
+import "./PopularBook.css";
 
-export default function PopularBook({data}) {
-    return (
-        <div>
-            <Container style={{ marginTop: '3em', marginBottom: '1.5em', color: 'var(--fontColorPrimary)' }}>
-                <h1>Popular Book</h1>
-            </Container>
-            <Container>
-                <div className="popular-book-container">
-                    {data?.map((value) => {
-                       return <PopularBookCard url={value?.Url} title={value?.Title} />
-                    })}
-                    
-                </div>
-            </Container>
+export default function PopularBook({ data, dataReadingList }) {
+  return (
+    <div>
+      <Container
+        style={{
+          marginTop: "3em",
+          marginBottom: "1.5em",
+          color: "var(--fontColorPrimary)",
+        }}
+      >
+        <h1>Popular Book</h1>
+      </Container>
+      <Container>
+        <div className="popular-book-container">
+          {data?.map((value) => {
+            return (
+              <PopularBookCard
+                url={value?.Url}
+                title={value?.Title}
+                bookData={value}
+                dataReadingList={dataReadingList}
+              />
+            );
+          })}
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
