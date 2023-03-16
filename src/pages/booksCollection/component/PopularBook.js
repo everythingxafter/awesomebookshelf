f9cf93c9e9409cf4668c939a75215669c01f3635
everythingxafter <112627979+everythingxafter@users.noreply.github.com>
2023-03-16 21:08:30 +0700

    fixing popular book scroll

--- src/pages/booksCollection/component/PopularBook.js
+++ src/pages/booksCollection/component/PopularBook.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import PopularBookCard from "./PopularBookCard";
 import "./PopularBook.css";
 import Loading from "../../../component/loading/Loading";
@@ -17,22 +17,28 @@ export default function PopularBook({ data, dataReadingList, loading }) {
         <h1>Popular Book</h1>
       </Container>
       <Container>
-        <div className="popular-book-container">
-          {data.length > 0 ? (
-            data?.map((value) => {
-              return (
-                <PopularBookCard
-                  url={value?.Url}
-                  title={value?.Title}
-                  bookData={value}
-                  dataReadingList={dataReadingList}
-                />
-              );
-            })
-          ) : (
-            <Loading seen={loading} />
-          )}
-        </div>
+        <Row>
+          <Col>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <div className="popular-book-container">
+                {data.length > 0 ? (
+                  data?.map((value) => {
+                    return (
+                      <PopularBookCard
+                        url={value?.Url}
+                        title={value?.Title}
+                        bookData={value}
+                        dataReadingList={dataReadingList}
+                      />
+                    );
+                  })
+                ) : (
+                  <Loading seen={loading} />
+                )}
+              </div>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
